1843ecd75cdff676e9eed13fda3f70f8ec51e8a7
joaopnk <joaopedronk22@gmail.com>
2022-06-21 22:13:28 -0300

    Dev. 21.06.22

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,7 +4,9 @@ import { Feather } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
 
 import {
-  Container
+  Container,
+  IconContainer,
+  InputText
 } from './styles';
 
 interface Props extends TextInputProps {
@@ -14,16 +16,22 @@ interface Props extends TextInputProps {
 
 export function Input({
   iconName,
+  ...rest
 }: Props){
   const theme = useTheme();
   
   return (
     <Container>
-      <Feather 
-        name={iconName}
-        size={24}
-        color={theme.colors.text_detail}
-      />
+      <IconContainer>
+        <Feather 
+          name={iconName}
+          size={24}
+          color={theme.colors.text_detail}
+        />
+      </IconContainer>
+
+      <InputText {...rest} />
+
     </Container>
   );
 }
